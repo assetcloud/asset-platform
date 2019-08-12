25f470268fe1b98e241bf358c3ec59ab5d606d90
Ricardo <wan54496@163.com>
2019-08-12 09:14:53 +0800

    准备测试分支流程

--- asset-platform-process/abpm-process-center/src/main/java/com/asset/entity/AsApplication.java
+++ asset-platform-process/abpm-process-center/src/main/java/com/asset/entity/AsApplication.java
@@ -2,9 +2,11 @@ package com.asset.entity;
 
 import java.time.LocalDateTime;
 import java.io.Serializable;
+import java.util.Date;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -20,6 +22,7 @@ import lombok.experimental.Accessors;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
+@TableName("as_application")
 public class AsApplication implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -50,17 +53,17 @@ public class AsApplication implements Serializable {
     /**
      * 创建时间
      */
-    private LocalDateTime createdTime;
+    private Date createdTime;
 
     /**
      * 失效时间
      */
-    private LocalDateTime disableTime;
+    private Date disableTime;
 
     /**
      * 删除时间
      */
-    private LocalDateTime removeTime;
+    private Date removeTime;
 
     public AsApplication() {
     }
