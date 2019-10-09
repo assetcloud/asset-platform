f3720114517414cbaadaa1fa384901f1632cd63b
Nfq226 <2639259789@qq.com>
2019-10-09 20:14:44 +0800

    nfq测试

--- asset-platform-process/abpm-process-center/src/main/java/com/asset/dto/SignStrategyDTO.java
+++ asset-platform-process/abpm-process-center/src/main/java/com/asset/dto/SignStrategyDTO.java
@@ -3,11 +3,14 @@ package com.asset.dto;
 import lombok.Data;
 
 /**
+ *
+ * 如果前台没有
+ *
  * 	"sign_strategy": {
- * 		"agree_rule": {
+ * 		"agree_rule": {   //这里表示会签节点的通过规则
  * 			"rule": {
- * 				"strategy": 0,
- * 				"num": 80
+ * 				"strategy": 0,      //当这个值为0代表 按人数选择通过
+ * 				"num": 80           //代表人数或者百分比
  *                        } 		},
  * 		"disagree_rule": {
  * 			"rule": {
