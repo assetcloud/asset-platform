7526173964deda6f4d33efc8e39d6e53a0bcecdb
Ricardo <wan54496@163.com>
2019-10-18 15:45:31 +0800

    修改为history模式

--- asset-platform-process/abpm-process-center/src/main/java/com/asset/FlowableApplication.java
+++ asset-platform-process/abpm-process-center/src/main/java/com/asset/FlowableApplication.java
@@ -6,7 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -33,4 +38,18 @@ public class FlowableApplication {
         springApplication.addListeners(new ApplicationStartup());
         springApplication.run(args);
     }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer(){
+        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+            @Override
+            public void customize(ConfigurableWebServerFactory factory) {
+                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+                factory.addErrorPages(error404Page);
+            }
+        };
+    }
+
+
+
 }
